f94d85942adb7d005cc3fbcfd6806847e1881432
Jimenez <a688291@cis.neustar.com>
2018-07-04 18:40:27 +0200

    arima and transformations

--- BasicTs.py
+++ BasicTs.py
@@ -23,6 +23,7 @@ from pandas.plotting import autocorrelation_plot
 from statsmodels.tsa.stattools import adfuller, acf, pacf,arma_order_select_ic
 import statsmodels.formula.api as smf
 import statsmodels.tsa.api as smt
+import statsmodels.tsa as smar
 import statsmodels.api as sm
 import scipy.stats as scs
 
@@ -32,16 +33,16 @@ import warnings
 import os
 warnings.filterwarnings("ignore")
 
-os.chdir("C:/Users/a688291/Documents/EDA_CRJR/Practice/Python/kg1")
+os.chdir("C:/Users/a688291/Documents/EDA_CRJR/Practice/Python/TsPractice")
 os.getcwd()
 
 # Import all of them 
-sales=pd.read_csv("input/sales_train_v2.csv")
-item_cat=pd.read_csv("input/item_categories.csv")
-item=pd.read_csv("input/items.csv")
-sub=pd.read_csv("input/sample_submission.csv")
-shops=pd.read_csv("input/shops.csv")
-test=pd.read_csv("input/test.csv")
+sales=pd.read_csv("../input/sales_train_v2.csv")
+item_cat=pd.read_csv("../input/item_categories.csv")
+item=pd.read_csv("../input/items.csv")
+sub=pd.read_csv("../input/sample_submission.csv")
+shops=pd.read_csv("../input/shops.csv")
+test=pd.read_csv("../input/test.csv")
 
 
 
@@ -253,6 +254,125 @@ plt.plot(nts)
 
 new_ts
 pd.Series(new_ts)
+
+
+
+
+##------------------------------------------------------
+##ARIMA Models with statsmodels.tsa
+
+def PlotSeries(ts, lags=12):
+    if not isinstance(ts, pd.Series):
+        ts = pd.Series(ts)
+    y = pd.Series.dropna(ts)
+    plt.figure(figsize=(10,8))
+    plt.subplot(311)
+    layout = (2,2)
+    ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+    acf_ax = plt.subplot2grid(layout, (1, 0))
+    pacf_ax = plt.subplot2grid(layout, (1, 1))
+    y.plot(ax=ts_ax)
+    smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=1, zero=False)
+    smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=1, zero=False)
+    plt.tight_layout()
+
+PlotSeries(ts)
+
+## Series transformation
+plt.figure(figsize=(12,7))
+ts = sales.groupby(["date"])["item_cnt_day"].sum()
+ts.plot()
+
+plt.figure(figsize=(12,7))
+log_ts = np.log(ts)
+log_ts.plot()
+
+plt.figure(figsize=(12,7))
+diff_ts=pd.Series.diff(log_ts, periods = 7)
+diff_ts.plot()
+
+plt.figure(figsize=(12,7))
+forw_ts = pd.Series(np.r_[log_ts.iloc[0],diff_ts.iloc[7:]].cumsum())
+forw_ts.plot()
+
+##
+plt.figure(figsize=(12,7))
+diff_ts_7 = np.log((pd.Series.dropna(diff_ts))+1)
+diff_ts_7.plot()
+diff_ts_7_1=pd.Series.diff(ts, periods =1)
+PlotSeries(diff_ts_7_1)
+
+##
+
+
+TSmodel = smar.arima_model.ARIMA(diff_ts_7,order=(0,1,1))
+TSmodelFit = TSmodel.fit(disp=0)
+print(TSmodelFit.summary())
+# plot residual errors
+residuals = pd.DataFrame(TSmodelFit.resid)
+residuals.plot()
+plt.show()
+residuals.plot(kind='kde')
+plt.show()
+print(residuals.describe())
+PlotSeries(residuals)
+
+
+np.r_[ts.iloc[0:7],diff_ts.iloc[7:]].cumsum()
+
+
+
+df_ts = df_ts.assign(diff_ts= diff_ts.values)
+df_ts = df_ts.assign(for_ts = df_forw_ts.values)
+
+
+pd.DataFrame.append
+
+
+diff_ts.plot()
+
+forw_ts = np.r_[ts.iloc[0],diff_ts.iloc[1:]].cumsum()
+pd.concat([pd.Series(ts.iloc[0]),forw_ts],ignore_index=True)
+forw_ts = pd.Series(forw_ts)
+forw_ts.plot()
+
+
+
+
+
+
+x, x_diff = df['A'].iloc[0], df['B'].iloc[1:]
+df['C'] = np.r_[x, x_diff].cumsum()
+
+new_ts.plot()
+new_ts=pd.Series.dropna(pd.Series.diff(new_ts, periods = -7))
+new_ts.plot()
+
+
+new_ts=pd.Series.dropna(pd.Series.diff(new_ts, periods = 52))
+PlotSeries(new_ts)
+
+ts = new_ts
+
+TSmodel = smar.arima_model.ARIMA(ts,order=(0,1,1))
+TSmodelFit = TSmodel.fit(disp=0)
+print(TSmodelFit.summary())
+# plot residual errors
+residuals = pd.DataFrame(TSmodelFit.resid)
+residuals.plot()
+plt.show()
+residuals.plot(kind='kde')
+plt.show()
+print(residuals.describe())
+PlotSeries(residuals)
+
+
+
+
+
+
+
+
 
 
 
